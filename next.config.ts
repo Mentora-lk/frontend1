ed7a0844f1d27ccff50c7d3418eaf9e1e90c2b4a
ranadheerado <dewminioshadhi1030@gmail.com>
2026-08-11 10:31:59 +0530

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      { source: "/admin/:path*", destination: "/dashboard/admin/:path*" },
+    ];
+  },
 };
 
 export default nextConfig;
